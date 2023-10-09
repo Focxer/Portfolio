9f4392ac43047a49fd43c671126449c0675faa9b
Focxer <you@example.com>
2023-10-08 22:55:10 -0300

    navbar link to element fix

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Menu, X } from "lucide-react"
 import Logo from '../../components/header/Logo'
+import { HashLink } from 'react-router-hash-link';
 
 function menuOpen(){
   document.getElementById("navBurger").setAttribute("class", "left-0 top-0 absolute h-72 w-screen bg-violet-500");
@@ -13,10 +14,10 @@ function Navbar() {
   return (
     <>
         <nav className="hidden md:flex gap-4 text-zinc-500 font-semibold">
-            <a href="#__sobremim" className="hover:text-violet-400">Sobre mim</a>
-            <a href="#__linguagens" className="hover:text-violet-400">Linguagens</a>
-            <a href="#__projetos" className="hover:text-violet-400">Projetos</a>
-            <a href="#__contato" className="hover:text-violet-400">Fale comigo</a>
+            <HashLink smooth to="#__sobremim" className="hover:text-violet-400">Sobre mim</HashLink>
+            <HashLink smooth to="#__linguagens" className="hover:text-violet-400">Linguagens</HashLink>
+            <HashLink smooth to="#__projetos" className="hover:text-violet-400">Projetos</HashLink>
+            <HashLink smooth to="#__contato" className="hover:text-violet-400">Fale comigo</HashLink>
         </nav>
 
         <nav className="md:hidden">
@@ -30,10 +31,10 @@ function Navbar() {
           </div>
 
           <nav className="flex flex-col text-zinc-50 font-semibold divide-y-2 divide-violet-300 shadow-xl" onClick={menuClose}>
-            <a href="#__sobremim" className="bg-violet-400 w-full text-center py-4">Sobre mim</a>
-            <a href="#__linguagens" className="bg-violet-400 w-full text-center py-4">Linguagens</a>
-            <a href="#__projetos" className="bg-violet-400 w-full text-center py-4">Projetos</a>
-            <a href="#__contato" className="bg-violet-400 w-full text-center py-4">Fale comigo</a>
+          <HashLink smooth to="#__sobremim" className="bg-violet-400 w-full text-center py-4">Sobre mim</HashLink>
+          <HashLink smooth to="#__linguagens" className="bg-violet-400 w-full text-center py-4">Linguagens</HashLink>
+          <HashLink smooth to="#__projetos" className="bg-violet-400 w-full text-center py-4">Projetos</HashLink>
+          <HashLink smooth to="#__contato" className="bg-violet-400 w-full text-center py-4">Fale comigo</HashLink>
           </nav>
         </div>
     </>
