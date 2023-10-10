a8ce4d3f5a266e6df431afedf8fd152436a35a1b
Focxer <you@example.com>
2023-10-10 00:00:48 -0300

    Wip label

---
+++ src/components/header/Wip.jsx
@@ -0,0 +1,11 @@
+function Wip() {
+  return (
+    <>
+      <div className="bg-red-500 font-bold text-zinc-50 text-sm px-1 py-2 rounded-lg absolute top-2 left-2 opacity-20 animate-pulse">
+        Em construção
+      </div>
+    </>
+  )
+}
+
+export default Wip
