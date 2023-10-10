23373796616e1d90c1e162078111ea98d6818e78
Focxer <you@example.com>
2023-10-10 00:09:45 -0300

    Wip label responsiveness

--- src/components/header/Wip.jsx
+++ src/components/header/Wip.jsx
@@ -1,11 +1,10 @@
 function Wip() {
   return (
     <>
-      <div className="bg-red-500 font-bold text-zinc-50 text-sm px-1 py-2 rounded-lg absolute top-2 left-2 opacity-20 animate-pulse">
+      <div className="bg-red-500 font-bold text-zinc-50 text-xs px-1 py-2 rounded-lg absolute top-4 left-4 hidden lg:flex opacity-20 animate-pulse">
         Em construção
       </div>
-    </>
-  )
+    </>  )
 }
 
 export default Wip
